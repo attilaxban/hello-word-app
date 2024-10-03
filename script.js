597d5769556845f618f175d9e6bd44c72b455102
attilaxban <banxattila@gmail.com>
2024-10-03 10:59:55 +0200

    test

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 const rootElement = document.getElementById('root');
 
 rootElement.insertAdjacentHTML('beforeend','<h1>Hello World!</h1>')
-
+rootElement.insertAdjacentHTML('beforeend','<h1>Hello World!</h1>')
