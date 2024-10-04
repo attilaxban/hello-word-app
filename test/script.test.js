5d7fb494e8f6f35d47341cab7902f3dfe329a20c
attilaxban <banxattila@gmail.com>
2024-10-04 11:12:44 +0200

    test

--- test/script.test.js
+++ test/script.test.js
@@ -11,6 +11,6 @@ describe('1. Test content of the element', function() {
 
   it('should have correct innerHTML in #root', function() {
     const rootElement = document.getElementById('root');
-    expect(rootElement.innerHTML).to.equal('<h1>Hello World!Test</h1>');
+    expect(rootElement.innerHTML).to.equal('<h1>Hello World!</h1>');
   });
 });
