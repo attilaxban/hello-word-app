bb7e9432161ff8e9d2d80b5b350e7d4ce5003454
attilaxban <banxattila@gmail.com>
2024-10-04 10:38:43 +0200

    test

---
+++ test/script.test.js
@@ -0,0 +1,16 @@
+import {expect} from 'chai'
+import { JSDOM } from 'jsdom';
+
+describe('1. Test content of the element', function() {
+  let dom, document;
+
+  before(function() {
+    dom = new JSDOM('<!DOCTYPE html><div id="root"><h1>Hello World!</h1></div>');
+    document = dom.window.document;
+  });
+
+  it('should have correct innerHTML in #root', function() {
+    const rootElement = document.getElementById('root');
+    expect(rootElement.innerHTML).to.equal('<h1>Hello World!</h1>');
+  });
+});
