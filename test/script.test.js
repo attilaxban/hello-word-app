ece254fe16f1b5cca18916d2e474a9c5b0ff1951
attilaxban <banxattila@gmail.com>
2024-10-04 11:00:45 +0200

    test

--- test/script.test.js
+++ test/script.test.js
@@ -5,12 +5,12 @@ describe('1. Test content of the element', function() {
   let dom, document;
 
   before(function() {
-    dom = new JSDOM('<!DOCTYPE html><div id="root"><h1>Hello World!!Test</h1></div>');
+    dom = new JSDOM('<!DOCTYPE html><div id="root"><h1>Hello World!</h1></div>');
     document = dom.window.document;
   });
 
   it('should have correct innerHTML in #root', function() {
     const rootElement = document.getElementById('root');
-    expect(rootElement.innerHTML).to.equal('<h1>Hello World!!Test</h1>');
+    expect(rootElement.innerHTML).to.equal('<h1>Hello World!</h1>');
   });
 });
